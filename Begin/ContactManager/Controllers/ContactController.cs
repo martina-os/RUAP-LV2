b586e4ffc5d40a931591da6e8f675ef5ba9a01ad
Martina Ostović <martina12.mo@gmail.com>
2024-11-14 00:27:22 +0100

    Modifying the Index View to Provide a GUI for Creating Contacts

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -18,5 +18,14 @@ namespace ContactManager.Controllers
         {
             return this.contactRepository.GetAllContacts();
         }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
     }
 }
